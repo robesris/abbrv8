93d21f5a33ea2bd944d0886d0673ae2f13c88190
Rob Esris <robesris@gmail.com>
2014-01-30 13:59:07 -0500

    Button stuff for cucumber feature

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -2,8 +2,9 @@ Given(/^I am on the homepage$/) do
   visit '/'
 end
 
-Given(/^I try to shorten "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I try to shorten "(.*?)"$/) do |long_url|
+  fill_in :abbrv8_url_long_url, with: long_url
+  click_on "Shorten"
 end
 
 Then(/^I should get a short url$/) do
